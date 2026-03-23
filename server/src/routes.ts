b632895e0da5e5e41aa726545952bd53176c9a1c
Wesleycords <Wesleycordeiro044@gmail.com>
2026-03-22 21:53:32 -0300

    feat(lint): Adds EditorConfig, ESLint, and Prettier for consistency

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,57 +1,10 @@
-import { FastifyPluginAsync, FastifyRequest, FastifyReply } from "fastify";
-import { prisma } from "./lib/prisma";
-
-interface IParams {
-  name: string;
-}
-
-const schemaForPost = {
-  body: {
-    type: "object",
-    required: ["username", "email"],
-    properties: {
-      username: { type: "string", minLength: 3 },
-      email: { type: "string", format: "email" },
-      age: { type: "number" },
-    },
-  },
-};
+import { FastifyPluginAsync, FastifyRequest, FastifyReply } from 'fastify';
+import { prisma } from './lib/prisma';
 
 const routes: FastifyPluginAsync = async (fastify, options) => {
-  fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
-    const user = await prisma.suppliers.fin;
-    return { hello: "world", user };
+  fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    return { hello: 'world' };
   });
-
-  fastify.get(
-    "/hello/:name",
-    async (
-      request: FastifyRequest<{ Params: IParams }>,
-      reply: FastifyReply,
-    ) => {
-      const { name } = request.params;
-      return { name };
-    },
-  );
-
-  fastify.post(
-    "/user",
-    { schema: schemaForPost },
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      const { username, age, email } = request.body as {
-        username: string;
-        age?: number;
-        email: string;
-      };
-
-      return reply.code(201).send({
-        id: 1,
-        message: `Usuário ${username} criado no Fedora!`,
-        age,
-        email,
-      });
-    },
-  );
 };
 
 export default routes;
