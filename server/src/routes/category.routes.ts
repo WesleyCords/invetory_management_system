fe31e66dee208ef6b8e3c0aaca60dfd047255aee
Wesleycords <Wesleycordeiro044@gmail.com>
2026-04-11 21:56:46 -0300

    feat: Config and add the ZOD

--- server/src/routes/category.routes.ts
+++ server/src/routes/category.routes.ts
@@ -1,7 +1,25 @@
-import { FastifyPluginAsync } from 'fastify';
+// import { FastifyPluginAsync } from 'fastify'; // was replace for plguin of ZOD
+import * as z from 'zod';
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 
-export const categoryRoutes: FastifyPluginAsync = async (fastify) => {
+export const categoryRoutes: FastifyPluginAsyncZod = async (fastify) => {
+  fastify.post(
+    '/categories',
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(3, 'Name must contain at least 3 characters.'),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const data = request.body;
+
+      reply.status(200).send(data);
+    },
+  );
+
   fastify.get('/categories', async () => {
-    return { hello: 'categories' };
+    return { hello: 'rota do categories' };
   });
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,35 @@
 import Fastify from 'fastify';
+
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+  hasZodFastifySchemaValidationErrors,
+} from 'fastify-type-provider-zod';
+
 import { prisma } from './lib/prisma.js';
 import { AppError } from './errors/appError.js';
 import { appRoutes } from './routes/index.js';
+import { ZodError } from 'zod';
 
 const PORT = 3000;
 
 const server = Fastify({
   logger: true,
-});
+}).withTypeProvider<ZodTypeProvider>(); // the type is now of ZOD
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
 
 server.setErrorHandler((err, req, reply) => {
+  if (hasZodFastifySchemaValidationErrors(err)) {
+    return reply.status(400).send({
+      name: 'error_validation',
+      message: 'Data validation error: ' + err.validation[0].message,
+      issues: err.validation,
+    });
+  }
+
   if (err instanceof AppError) {
     return reply.status(err.statusCode).send({
       status: 'error',
@@ -24,6 +44,7 @@ server.setErrorHandler((err, req, reply) => {
     message: 'Error Internal Server',
   });
 });
+
 server.register(appRoutes, { prefix: '/api/v1' });
 
 const start = async () => {
@@ -32,7 +53,7 @@ const start = async () => {
     console.log('BANCO DE DADOS CONECTADO COM SUCESSO!');
 
     await server.listen({ port: PORT, host: '0.0.0.0' });
-    console.log(`SERVIDOR SUBIU: http://localhost:${PORT}`);
+    console.log(`SERVIDOR SUBIU: http://localhost:${PORT}/api/v1`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
